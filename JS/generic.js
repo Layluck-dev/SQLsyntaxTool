ef94ba80a5e81f9d16c21b674c67bf26ada4e962
Jeroen <55247019+Layluck-dev@users.noreply.github.com>
2019-09-12 18:03:57 +0200

    Fixed commentary syntax error

--- JS/generic.js
+++ JS/generic.js
@@ -1,36 +1,36 @@
 $(document).ready(function(){
 	
-	#Input value to convert
+	//Input value to convert
 	var Conv = "";
 	
-	#Empty the input field just incase there is something there
-	#that isn't supposed to be there, or to reset after use.
+	//Empty the input field just incase there is something there
+	//that isn't supposed to be there, or to reset after use.
 	$("#inputField").val("");
 	
 	$("#convert").click(function(){
 		
-		#Assign the input value and add the beginning and end syntax
+		//Assign the input value and add the beginning and end syntax
 		Conv = $("#inputField").val();
 		Conv = '("' + Conv + '")';
 		
-		#Add after new line a new div and marker and add after other 
-		#odd spacing markers
+		//Add after new line a new div and marker and add after other 
+		//odd spacing markers
 		Conv = Conv.replace(/\n/g, '"),<div class="bit"> >SPLITHERE<("');
 		Conv = Conv.replace(/\t/g, '", >SPLITHERE<"');
 		Conv = Conv.replace(/\:/g, '", >SPLITHERE<"');
 		Conv = Conv.replace(/  +/g, '", >SPLITHERE<"');
 		Conv = Conv.replace(/ \- +/g, '", >SPLITHERE<"');
 		
-		#Split the string into array based on markers
+		//Split the string into array based on markers
 		var bits = Conv.split('>SPLITHERE<');
 		
-		#Prime the output field
+		//Prime the output field
 		$("#outputField").html("<div class='bit'>");
 		
-		#Append every index to the ouput field
+		//Append every index to the ouput field
 		for(var i = 0; i < bits.length; i++){
 			
-			#Filter out any images urls if they were copied along
+			//Filter out any images urls if they were copied along
 			if(bits[i].toLowerCase().indexOf(".png") >= 0){
 				bits[i] = "";
 			}
@@ -38,7 +38,7 @@ $(document).ready(function(){
 			$("#outputField").append(bits[i]);
 		}
 		
-		#Close the final div on the output field
+		//Close the final div on the output field
 		$("#outputField").append("</div>");
 
 	});
